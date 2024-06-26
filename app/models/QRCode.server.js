b7b0e2f463583f2408dd3bcd654e6de318537d70
maeci <cimafrancamae@gmail.com>
2024-06-26 22:19:04 +0800

    add qrcode

--- app/models/QRCode.server.js
+++ app/models/QRCode.server.js
@@ -1,7 +1,6 @@
 import qrcode from "qrcode";
 import invariant from "tiny-invariant";
 import db from "../db.server";
-import { title } from "process";
 
 export async function getQRCode(id, graphql) {
   const qrCode = await db.qRCode.findFirst({ where: { id } });
