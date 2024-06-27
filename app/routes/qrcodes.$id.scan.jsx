36cc378c688db90089978932cd97865dfa67c0bf
maeci <cimafrancamae@gmail.com>
2024-06-27 11:11:47 +0800

    refactor qrcodes scan

--- app/routes/qrcodes.$id.scan.jsx
+++ app/routes/qrcodes.$id.scan.jsx
@@ -1,14 +1,14 @@
-import invariant from 'tiny-invariant';
+import { redirect } from "@remix-run/node";
+import invariant from "tiny-invariant";
+import db from "../db.server";
 
-import db from "../db.server"
-import { redirect } from "react-router";
 import { getDestinationUrl } from "../models/QRCode.server";
 
 export const loader = async ({ params }) => {
   invariant(params.id, "Could not find QR code destination");
 
   const id = Number(params.id);
-  const qrCode = await db.qRCode.findFirst({ where: { id }});
+  const qrCode = await db.qRCode.findFirst({ where: { id } });
 
   invariant(qrCode, "Could not find QR code destination");
 
@@ -18,4 +18,4 @@ export const loader = async ({ params }) => {
   });
 
   return redirect(getDestinationUrl(qrCode));
-}
+};
